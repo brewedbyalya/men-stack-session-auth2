c07fe029cbea0097c25bbafc3f731593ae81c38a
alya <aliya2002mah@gmail.com>
2025-07-15 13:55:05 +0300

    started

---
+++ server.js
@@ -0,0 +1,34 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+
+let port;
+if (process.env.PORT) {
+  port = process.env.PORT;
+} else {
+  port = 3000;
+}
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+// Middleware 
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(morgan('dev'));
+
+// Routes
+
+
+// Server
+app.listen(port, () => {
+  console.log(`The express app is ready on port ${port}!`);
+});
