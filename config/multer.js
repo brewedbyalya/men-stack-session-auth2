01b1d6bdf22d8774b7259774890faf8ec483cd81
alya <aliya2002mah@gmail.com>
2025-07-17 16:29:41 +0300

    fixes

--- config/multer.js
+++ config/multer.js
@@ -2,11 +2,11 @@ const multer = require('multer');
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const cloudinary = require('./cloudinary');
 
-const storage = CloudinaryStorage({
+const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
         folder: 'men-stack-session-auth2-listings',
-        allowed_formats: ['jpg','jpeg','png'],
+        allowed_formats: ['jpg','jpeg','png']
     }
 })
 
