ffbd568ae368f11e97acd9aa3dbd47a4bd34fd0c
alya <aliya2002mah@gmail.com>
2025-07-17 14:58:06 +0300

    some additions, needs fixing

--- controllers/listingController.js
+++ controllers/listingController.js
@@ -3,9 +3,10 @@ const router = express.Router()
 const listings = require('../models/listing')
 const isSignedIn = require('../middleware/is-signed-in')
 
+
 // new - get
 router.get('/new', isSignedIn, (req, res) => {
-    res.render('listing/new.ejs', { user: req.session.user })
+    res.render('listing/new.ejs')
 })
 
 // new - post
@@ -15,137 +16,70 @@ router.post('/', isSignedIn, async (req, res) => {
         await listings.create(req.body)
         res.redirect('/listing')
     } catch (error) {
-        console.error(error)
-        res.status(500).render('error', { 
-            message: 'Failed to create listing',
-            user: req.session.user 
-        })
+        console.log(error)
+        res.send('Something went wrong')
     }
 })
 
 // index
 router.get('/', async (req, res) => {
-    try {
-        const foundListings = await listings.find().populate('seller')
-        res.render('listing/index.ejs', { 
-            foundListings: foundListings,
-            user: req.session.user 
-        })
-    } catch (error) {
-        console.error(error)
-        res.status(500).render('error', { 
-            message: 'Failed to load listings',
-            user: req.session.user 
-        })
-    }
+    const foundListings = await listings.find()
+    console.log(foundListings)
+    res.render('listing/index.ejs', { foundListings: foundListings })
 })
 
 // show
 router.get('/:listingsId', async (req, res) => {
     try {
-        const foundListing = await listings.findById(req.params.listingsId).populate('seller')
-        if (!foundListing) {
-            return res.status(404).render('error', { 
-                message: 'Listing not found',
-                user: req.session.user 
-            })
-        }
-        res.render('listing/show.ejs', { 
-            foundListing: foundListing,
-            user: req.session.user 
-        })
+        const foundListing = await listings.findById(req.params.listingsId).populate('seller').populate('comments.author')
+        console.log(foundListing)
+        res.render('listing/show.ejs', { foundListing: foundListing })
     } catch (error) {
-        console.error(error)
-        res.status(500).redirect('/listing')
+        console.log(error)
+        res.redirect('/')
     }
 })
 
 // delete
-router.delete('/:listingsId', isSignedIn, async (req, res) => {
-    try {
-        const foundListing = await listings.findById(req.params.listingsId).populate('seller')
-        if (!foundListing) {
-            return res.status(404).render('error', { 
-                message: 'Listing not found',
-                user: req.session.user 
-            })
-        }
-        if (!foundListing.seller._id.equals(req.session.user._id)) {
-            return res.status(403).render('error', { 
-                message: 'Not authorized',
-                user: req.session.user 
-            })
-        }
+router.delete('/:listingId', isSignedIn, async (req, res) => {
+    const foundListing = await listings.findById(req.params.listingsId).populate('seller')
+    if (foundListing.seller._id.equals(req.session.user._id)) {
         await foundListing.deleteOne()
-        res.redirect('/listing')
-    } catch (error) {
-        console.error(error)
-        res.status(500).render('error', { 
-            message: 'Failed to delete listing',
-            user: req.session.user 
-        })
+        return res.redirect('/listing')
     }
+    return res.send('Not authorized')
 })
 
-// edit - get
+// edit
 router.get('/:listingsId/edit', isSignedIn, async (req, res) => {
-    try {
-        const foundListing = await listings.findById(req.params.listingsId).populate('seller')
-        if (!foundListing) {
-            return res.status(404).render('error', { 
-                message: 'Listing not found',
-                user: req.session.user 
-            })
-        }
-        if (!foundListing.seller._id.equals(req.session.user._id)) {
-            return res.status(403).render('error', { 
-                message: 'Not authorized',
-                user: req.session.user 
-            })
-        }
-        res.render('listing/edit.ejs', { 
-            foundListing: foundListing,
-            user: req.session.user 
-        })
-    } catch (error) {
-        console.error(error)
-        res.status(500).redirect('/listing')
-    }
+    const foundListing = await listings.findById(req.params.listingsId).populate('seller')
+
+    if (foundListing.seller._id.equals(req.session.user._id)) {
+        return res.render('listing/edit.ejs', { foundListing: foundListing} )
+    } 
+        return res.send('Not authorized')
+   
+
 })
 
-// edit - put
 router.put('/:listingsId', isSignedIn, async (req, res) => {
-    try {
-        const foundListing = await listings.findById(req.params.listingsId).populate('seller')
-        if (!foundListing) {
-            return res.status(404).render('error', { 
-                message: 'Listing not found',
-                user: req.session.user 
-            })
-        }
-        if (!foundListing.seller._id.equals(req.session.user._id)) {
-            return res.status(403).render('error', { 
-                message: 'Not authorized',
-                user: req.session.user 
-            })
-        }
+    const foundListing = await listings.findById(req.params.listingsId).populate('seller')
+    if (foundListing.seller._id.equals(req.session.user._id)) {
         await listings.findByIdAndUpdate(req.params.listingsId, req.body, { new: true })
-        res.redirect(`/listing/${req.params.listingsId}`)
-    } catch (error) {
-        console.error(error)
-        res.status(500).render('error', { 
-            message: 'Failed to update listing',
-            user: req.session.user 
-        })
-    }
+        return res.redirect(`/listing/${req.params.listingsId}`)
+    } 
+        return res.send('Not authorized')
+   
 })
 
-// comments
-router.post('/:listingsId/comments', async (req,res) => {
-    const foundListing = await listings.findById(req.params.listingsId);
-    req.body.author = req.session.user._id;
-    res.send(`Posting comment on listing: ${req.params.listingsId}`);
-}
-)
+// comment
+router.post('/:listingsId/comments', isSignedIn, async (req, res) => {
+    const foundListing = await listings.findById(req.params.listingsId)
+    req.body.author = req.session.user._id
+    console.log(req.body)
+    foundListing.comments.push(req.body)
+    await foundListing.save()
+    res.redirect(`/listing/${req.params.listingsId}`)
+})
 
 module.exports = router
